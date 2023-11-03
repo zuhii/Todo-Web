b139a8b220fc9080295cab7c530f9abd84d91f1f
Juhee <juhee01176@gmail.com>
2023-11-03 23:33:26 +0900

    commit

---
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "todo")
+public class TodoEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id; //오브젝트의 아이디
+    private String userId; //오브젝트를 생성한 사용잦의 아이디
+    private String title; //Todo타이틀 (ex:운동하기)
+    private boolean done; //true - todo를 완료한 경우(checked)
+}
---
+++ src/main/java/com/example/demo/persistance/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.persistance;
+
+import com.example.demo.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String>{}
